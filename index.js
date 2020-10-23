dd4bad86ba72b23203d98d2add597a0406c20fb6
BEAST <49040508+BEASTSM96@users.noreply.github.com>
2020-10-23 22:59:31 +0100

    Added command handler  (#1)
    * Added a Command Handler.
    
    * fixed token
    
    * Update index.js
    
    Co-authored-by: Chris Gong <chris_gong@yahoo.com>

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-require('dotenv').config();
+require( 'dotenv' ).config();
 
-const Discord = require('discord.js');
+const Discord = require( 'discord.js' );
 const client = new Discord.Client();
 
+const fs = require( 'fs' );
+
+const _PREFIX = '!';
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync( './commands/' ).filter(file => file.endsWith( '.js' ));
+for( const file of commandFiles ) {
+    const commands = require( `./commands/${file}` );
+
+    client.commands.set( commands.name, commands );
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log( 'Ready!' );
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith('!') || message.author.bot) return;
+    if (!message.content.startsWith( _PREFIX ) || message.author.bot) return;
 
     const args = message.content.slice(1).trim().split(' ');
     const command = args.shift().toLowerCase();
-	if (command === 'help') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send(`help ${args}`);
+
+    if ( command === 'ping' ) {
+	// send back "Pong." to the channel the message was sent in
+        client.commands.get( 'ping' ).execute(message, args);
     }
-    else if (command === 'test') {
-        message.channel.send(`test`);
+
+    if ( command === 'help' ) {
+        message.channel.send(`help ${args}`);
     }
 });
 
